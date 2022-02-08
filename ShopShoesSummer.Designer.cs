285bcf0c3ff93342ea6c25541660f35e4188a7b2
Alexandr Roslyakov <99288384+KingDog25@users.noreply.github.com>
2022-02-09 02:52:34 +0300

    Add files via upload

---
+++ ShopShoesSummer.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace _23_variant_CSharp_5_2_2
+{
+    partial class ShopShoesSummer
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBoxSummerShoes = new System.Windows.Forms.CheckBox();
+            this.buttonShow = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBoxSummerShoes
+            // 
+            this.checkBoxSummerShoes.AutoSize = true;
+            this.checkBoxSummerShoes.Location = new System.Drawing.Point(12, 49);
+            this.checkBoxSummerShoes.Name = "checkBoxSummerShoes";
+            this.checkBoxSummerShoes.Size = new System.Drawing.Size(459, 23);
+            this.checkBoxSummerShoes.TabIndex = 9;
+            this.checkBoxSummerShoes.Text = "Вывести сведения о летней детской обуви и ее среднюю стоимость";
+            this.checkBoxSummerShoes.UseVisualStyleBackColor = true;
+            // 
+            // buttonShow
+            // 
+            this.buttonShow.Location = new System.Drawing.Point(769, 49);
+            this.buttonShow.Name = "buttonShow";
+            this.buttonShow.Size = new System.Drawing.Size(120, 26);
+            this.buttonShow.TabIndex = 8;
+            this.buttonShow.Text = "Показать";
+            this.buttonShow.UseVisualStyleBackColor = true;
+            this.buttonShow.Click += new System.EventHandler(this.buttonShow_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.70909F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(373, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 32);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Обувной магазин";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-6, 81);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 47;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1096, 708);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.163636F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(26, 813);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 19);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Средняя стоимость обуви: ";
+            // 
+            // ShopShoesSummer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 852);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBoxSummerShoes);
+            this.Controls.Add(this.buttonShow);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "ShopShoesSummer";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBoxSummerShoes;
+        private System.Windows.Forms.Button buttonShow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ShopShoesSummer.cs
@@ -0,0 +1,110 @@
+﻿/*Задача 5.2.1 Условие: Имеются следующие данные о товарах обувного магазина: наименование(ботинки, сапоги, босоножки т.д.), 
+тип(мужская, женская или детская обувь), сезон, цвет, размеры, стоимость.Вывести сведения о летней детской обуви и ее среднюю стоимость. */
+
+/*Task 5.2.1 Condition: There is the following data about the goods of a shoe store: name (boots, boots, sandals, etc.),
+type (men's, women's or children's shoes), season, color, size, cost. Display information about summer children's shoes and their average cost. */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _23_variant_CSharp_5_2_2
+{
+    public partial class ShopShoesSummer : Form
+    {
+        public ShopShoesSummer()
+        {
+            InitializeComponent(); 
+        }
+            public enum NameShoes : byte
+        {
+            Ботинки, Сапоги, Босоножки, Сандали, Туфли, Кроссовки, Кеды, Шлепанцы
+        }
+        public enum Season : byte
+        {
+            Весна, Лето, Зима, Осень
+        }
+        public enum TypeofShoes : byte
+        {
+            Мужская, Женская, Детская
+        }
+
+        public enum ColorShoes : byte
+        {
+            Бежевый, Белый, Бордовый, Зеленый, Желтый, Золотой, Оранжевый, Коричневый,
+            Серый, Черный, Синий, Красный, Фиолетовый
+        }
+        double costShoes;                                                        //стоимость
+        byte size;                                                              //размеры
+        double averVal = 0;                                                       //среднее значение
+
+        private void buttonShow_Click(object sender, EventArgs e)
+        {
+            int shoesCount = 30;                                                 //кол-во товаров
+            int paramColumn = 6;                                                    //кол-во столбцов (параметров)
+            double summGrades = 0;                                                  //сумма стоимости
+            dataGridView1.RowCount = shoesCount;                                       //кол-во строк
+            dataGridView1.ColumnCount = paramColumn;                                                 //столбцов
+            dataGridView1.Columns[0].HeaderText = "Наименование";
+            dataGridView1.Columns[1].HeaderText = "Сезон";
+            dataGridView1.Columns[2].HeaderText = "Тип";
+            dataGridView1.Columns[3].HeaderText = "Цвет";
+            dataGridView1.Columns[4].HeaderText = "Размер";
+            dataGridView1.Columns[5].HeaderText = "Стоимость";
+            Random rnd = new Random();
+            int LengthName = Enum.GetNames(typeof(ShopShoesSummer.NameShoes)).Length;
+            int LengthSeason = Enum.GetNames(typeof(ShopShoesSummer.Season)).Length;
+            int LengthType = Enum.GetNames(typeof(ShopShoesSummer.TypeofShoes)).Length;
+            int LengthColorShoes = Enum.GetNames(typeof(ShopShoesSummer.ColorShoes)).Length;
+            ShopShoesSummer.NameShoes nameShoes = (ShopShoesSummer.NameShoes)rnd.Next(LengthName);
+            ShopShoesSummer.Season seasonShoes = (ShopShoesSummer.Season)rnd.Next(LengthSeason);
+            ShopShoesSummer.TypeofShoes typeShoes = (ShopShoesSummer.TypeofShoes)rnd.Next(LengthType);
+            ShopShoesSummer.ColorShoes colorShoes = (ShopShoesSummer.ColorShoes)rnd.Next(LengthColorShoes);
+            int rowsCount = shoesCount;
+            int columnCount = paramColumn;
+            string[,] tableShoes = new string[rowsCount, columnCount];
+            for (int count_row = 0; count_row < shoesCount; count_row++)           
+            {
+                seasonShoes = (Season)rnd.Next(LengthSeason);
+                if (checkBoxSummerShoes.Checked && ((int)seasonShoes != 1) && ((int)typeShoes ==0) && ((int)typeShoes == 1)) 
+                {
+                    count_row--;
+                    continue;
+                }
+                nameShoes = (NameShoes)rnd.Next(LengthName);
+                tableShoes[count_row, 0] = Convert.ToString(nameShoes);
+                dataGridView1.Rows[count_row].Cells[0].Value = tableShoes[count_row, 0];
+
+                tableShoes[count_row, 1] = Convert.ToString(seasonShoes);
+                dataGridView1.Rows[count_row].Cells[1].Value = tableShoes[count_row, 1];
+
+                typeShoes = (TypeofShoes)rnd.Next(LengthType);
+                tableShoes[count_row, 2] = Convert.ToString(typeShoes);
+                dataGridView1.Rows[count_row].Cells[2].Value = tableShoes[count_row, 2];
+
+                colorShoes = (ColorShoes)rnd.Next(LengthColorShoes);
+                tableShoes[count_row, 3] = Convert.ToString(colorShoes);
+                dataGridView1.Rows[count_row].Cells[3].Value = tableShoes[count_row, 3];
+
+                size = (byte)rnd.Next(34, 46);
+                tableShoes[count_row, 4] = Convert.ToString(size);
+                dataGridView1.Rows[count_row].Cells[4].Value = tableShoes[count_row, 4];
+
+                costShoes = rnd.NextDouble()*1000;
+                tableShoes[count_row, 5] = costShoes.ToString("0 руб. ## коп.");
+                dataGridView1.Rows[count_row].Cells[5].Value = tableShoes[count_row, 5];
+                summGrades += costShoes;
+
+            }
+            averVal = summGrades / shoesCount;
+            label2.Text += averVal.ToString("0 руб. ## коп.");
+            averVal = 0;                                                                            //обнуляем переменные
+            summGrades = 0;
+        }
+    }
+        }
